daed010d6f9824eaa0ffda814ead91fac7f3b709
hakjoo <hakjoo@mz.co.kr>
2023-06-15 16:40:43 +0900

    keyboard visilbility check library 도입, login background 분리

--- lib/presentation/Login.dart
+++ lib/presentation/Login.dart
@@ -14,28 +14,102 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Stack(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              color: Colors.blue,
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.3,
-              child: Text(
-                "Safety CS",
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 30,
-                    color: Colors.white),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          LoginBackground(),
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.fromLTRB(50, 100, 50, 50),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      blurRadius: 5.0,
+                      spreadRadius: 0.0,
+                      offset: const Offset(0, 3),
+                    )
+                  ]),
+              padding: EdgeInsets.all(32.0),
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      TextField(
+                        controller: userIdController,
+                        style: TextStyle(
+                          color: Colors.blue.shade500,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: '아이디',
+                          hintStyle:
+                              TextStyle(fontSize: 13, color: Colors.grey),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: "패스워드",
+                          hintStyle:
+                              TextStyle(fontSize: 13, color: Colors.grey),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("로그인"),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        child: const Text("회원가입"),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterPage()),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            Container(
-              alignment: Alignment.bottomCenter,
-              padding: EdgeInsets.fromLTRB(50, 0, 50, 30),
-              child: Container(
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.fromLTRB(50, 0, 50, 50),
+            child: Container(
+                alignment: Alignment.center,
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.all(
@@ -49,104 +123,44 @@ class _LoginPageState extends State<LoginPage> {
                         offset: const Offset(0, 3),
                       )
                     ]),
-                padding: EdgeInsets.all(32.0),
-                height: MediaQuery.of(context).size.height * 0.7,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        TextField(
-                          controller: userIdController,
-                          style: TextStyle(
-                            color: Colors.blue.shade500,
-                          ),
-                          decoration: const InputDecoration(
-                            hintText: "아이디",
-                            hintStyle:
-                                TextStyle(fontSize: 13, color: Colors.grey),
-                            filled: true,
-                            fillColor: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        TextField(
-                          controller: passwordController,
-                          obscureText: true,
-                          style: TextStyle(
-                            color: Colors.blue,
-                          ),
-                          decoration: const InputDecoration(
-                            hintText: "패스워드",
-                            hintStyle:
-                                TextStyle(fontSize: 13, color: Colors.grey),
-                            filled: true,
-                            fillColor: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("로그인"),
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blue,
-                            foregroundColor: Colors.white),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          child: const Text("회원가입"),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const RegisterPage()),
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+                padding: EdgeInsets.all(10.0),
+                height: 70,
+                child: Text("소셜 로그인")),
+          )
+        ],
       ),
     );
   }
 }
 
-class MenuWidget extends StatelessWidget {
-  const MenuWidget({super.key});
+class LoginBackground extends StatelessWidget {
+  const LoginBackground({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-
-    return ClipRRect(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.5,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(20),
+        ),
+      ),
       child: Container(
-        color: Colors.deepOrange,
-        width: width,
-        height: height / 3,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
-          child: Column(
-            children: <Widget>[
-              Icon(Icons.keyboard_arrow_up, size: 20),
-              Text("HELLO")
-            ],
-          ),
+        padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Container(
+              height: 120,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/logo.png'),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
